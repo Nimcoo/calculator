fb69738420f4560016d73c78e17ec017750ec8af
NIMCOO <nimco77001@gmail.com>
2023-11-05 14:07:45 +0000

    Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,11 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+fuctioncalculator(Num1,NUM2){
+
+    console. log(3,8, "+"); // output:11
+    console. log(3,8, "-"); // output:5
+    console. log(3,8, "*"); // output:24
+    console. log(3,8, "/"); // output:0.375
+    console. log(3,8, "%");// invalid operator
+    
+   }
